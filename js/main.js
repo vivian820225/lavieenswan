765138e96c4d145628112a3bfedca22b9804bdfb
Vivian.C <vivian820225@gmail.com>
2022-11-04 00:21:56 +0800

    [Fix] 依需求修改
    1. 空拍橫向滑動
    2. 文案修改

--- js/main.js
+++ js/main.js
@@ -82,3 +82,44 @@ const swiperS8 = new Swiper("#swiper-s8", {
     prevEl: "#swiper-s8 .swiper-button-prev",
   },
 });
+
+let timer = null;
+let callback = ([entry]) => {
+  const toast = document.querySelector(".toast");
+  if (entry.isIntersecting) {
+    toast.classList.add("active");
+    timer = setTimeout(() => {
+      toast.classList.remove("active");
+    }, 5 * 1000);
+    return;
+  }
+  clearTimeout(timer);
+  toast.classList.remove("active");
+};
+
+const options = { threshold: 0 };
+
+const observer = new IntersectionObserver(callback, options);
+
+const section = document.querySelector(".s1");
+const dragContainer = document.querySelector(".horizontal-scroll-container");
+
+function initPositionImg() {
+  const dragImg = document.querySelector(".scroll-img");
+  const positionLeft = dragImg.clientWidth / 2 - dragContainer.clientWidth;
+  dragContainer.scrollLeft = positionLeft;
+}
+
+const ImgObserver = new ResizeObserver(([entry]) => {
+  if (entry && entry.target) {
+    initPositionImg();
+  }
+});
+
+window.addEventListener("load", function () {
+  const width = window.innerWidth;
+  if (width < 1024) {
+    observer.observe(section);
+    ImgObserver.observe(dragContainer, { box: "border-box" });
+  }
+});
